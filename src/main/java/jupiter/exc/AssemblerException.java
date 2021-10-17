c2b90be1757f034ea09864408ddd93127ab9154d
CapitanNevo <92693181+CapitanNevo@users.noreply.github.com>
2021-10-17 23:36:08 +0300

    Update AssemblerException.java

--- src/main/java/jupiter/exc/AssemblerException.java
+++ src/main/java/jupiter/exc/AssemblerException.java
@@ -1,33 +1,56 @@
-/*
-Copyright (C) 2018-2019 Andres Castellanos
+.globl __start; Экспортирование глобальной переменной (нужно, чтобы вызывающий (ОС) знал, шде начинается программа)
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+.rodata; Секция данных. Здесь хранятся глобальные переменные
+  fmtn: .string "t0 IS:"
+  msg: .string "Input number:"
+  ext: .string "Exiting"
+  nl: .string "\n"
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+.text; Секция комманд
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-*/
+read_n:;Функция, читает строку, возвращает в a1
+  li a0, 4 ;Встроенный вызов: вывести строку на консоль
+  la a1, msg; Загрузить строку msg
+  ecall; Вызвать системный вызов
+  
+  li a0, 5; Системный вызов прочитать число
+  ecall
+  ret
 
-package jupiter.exc;
+prnt_s:; Функция, печатает строку из регистра a0: printf("%s\n", a0)
+  mv a1, a0
+  li a0, 4;
+  ecall
 
+  ;print \n
+  li a0, 4;
+  la a1, nl
+  ecall
+  ret
+  
+revert:;Переворачивает десятичное представление числа
+;  div t2, t1, t0; t2 = t1 / t0  
+  mv t1, a0; t1 = a0
+  li t6, 10
+  
+  loop:
+    rem t3, t1, t6; t3 = t1 %t6
+    ;t1 = t1 / t6
+    div t5, t1, t6
+    mv t1, t5
+    ;print t1
+    mv a1, t3
+    li a0, 1
+    ecall
+  bgtz t1, loop
+  
+  ret
 
-/** Assembler errors. */
-public class AssemblerException extends JupiterException {
-
-  /**
-   * Creates a new assembler exception.
-   *
-   * @param msg assembler exception message
-   */
-  public AssemblerException(String msg) {
-    super(msg);
-  }
-
-}
+__start:; Точка входа в программу
+  call read_n
+  call revert
+  
+  li a0, 10;Завершить программу  exit(0)
+  ecall
+  ret
+  
